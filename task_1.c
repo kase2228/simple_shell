69be8aaadca5bea28ab6b1d63b28cc32cbc429f0
etel <etelethiopia@gmail.com>
2023-12-13 10:47:28 -0800

    CLI

--- task_1.c
+++ task_1.c
@@ -1,67 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-
-#define MAX_COMMAND_LENGTH 100
-
-/**
- * execute_command - executes the given command
- * @command: the command to execute
- */
-void execute_command(char *command)
-{
-    char *arguments[MAX_COMMAND_LENGTH / 2 + 1];
-    char *token;
-    int i = 0;
-
-    // Tokenize the command string
-    token = strtok(command, " ");
-    while (token != NULL && i < MAX_COMMAND_LENGTH / 2) {
-        arguments[i] = token;
-        token = strtok(NULL, " ");
-        i++;
-    }
-    arguments[i] = NULL;
-
-    // Check if the command exists in the PATH
-    if (access(arguments[0], X_OK) == 0) {
-        // Fork a child process
-        pid_t pid = fork();
-        if (pid == -1) {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        } else if (pid == 0) { // Child process
-            execve(arguments[0], arguments, NULL);
-            perror("execve");
-            exit(EXIT_FAILURE);
-        } else { // Parent process
-            wait(NULL);
-        }
-    } else {
-        printf("Command not found: %s\n", arguments[0]);
-    }
-}
+#include <sys/wait.h>
 
 /**
- * main - entry point
- * Return: 0 on success, otherwise 1
+ * main - Simple UNIX command line interpreter
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-    char command[MAX_COMMAND_LENGTH];
+char *line = NULL;
+size_t len = 0;
+ssize_t read;
+pid_t child_pid;
+int status;
 
-    while (1) {
-        printf("$ ");
-        fgets(command, MAX_COMMAND_LENGTH, stdin);
-        command[strcspn(command, "\n")] = '\0'; // Remove the trailing newline
-
-        if (strcmp(command, "exit") == 0) {
-            break;
-        }
-
-        execute_command(command);
-    }
-
-    return 0;
+while (1)
+{
+printf("$ ");
+read = getline(&line, &len, stdin);
+if (read == -1)
+break;
+if (line[read - 1] == '\n')
+line[read - 1] = '\0';
+child_pid = fork();
+if (child_pid == -1)
+perror("Error");
+if (child_pid == 0)
+{
+if (execve(line, NULL, NULL) == -1)
+perror("Error");
+exit(EXIT_FAILURE);
+}
+else
+wait(&status);
+}
+free(line);
+exit(EXIT_SUCCESS);
 }
