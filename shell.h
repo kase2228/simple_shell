1eaaa36e7d5278576d27a8992c5d15a360d74013
etel <etelethiopia@gmail.com>
2023-12-13 10:49:33 -0800

    header

---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(void);
+
+#endif /* SIMPLE_SHELL_H */
